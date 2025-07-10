532b5519624fb694b6ccdc6b03340115c883d7d6
Cybermartins <biobelemartins24@yahoo.com>
2025-07-10 10:46:57 +0100

    Hero Section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import './Hero.css'
+import './Hero.css';
+import hero_pic from '../../assets/restauranfood.jpg'
 
 const Hero = () => {
   return (
-    <div className='hero'>
-
+    <div className='hero container'>
+      <div className="hero-text">
+        <h1>Little Lemon</h1>
+        <h2>Chicago</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <button className='btn'>Reserve a Table</button>
+      </div>
+      <img src={hero_pic} alt='' className='hero-pic' />
     </div>
   )
 }
